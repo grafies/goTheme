f39ad7dbf70448f7fb28f5f86d7b37ccea966320
Grafies <11865692+grafies@user.noreply.gitee.com>
2023-06-15 09:21:34 +0000

    theme.go
    Signed-off-by: Grafies <11865692+grafies@user.noreply.gitee.com>

---
+++ go.mod
@@ -0,0 +1,13 @@
+module gitee.com/grafies/goTheme
+
+go 1.20
+
+require (
+	fyne.io/fyne/v2 v2.3.5 // indirect
+	github.com/fredbi/uri v0.1.0 // indirect
+	github.com/srwiley/oksvg v0.0.0-20220731023508-a61f04f16b76 // indirect
+	github.com/srwiley/rasterx v0.0.0-20210519020934-456a8d69b780 // indirect
+	golang.org/x/image v0.3.0 // indirect
+	golang.org/x/net v0.0.0-20220722155237-a158d28d115b // indirect
+	golang.org/x/text v0.6.0 // indirect
+)
---
+++ theme.go
@@ -0,0 +1,38 @@
+package theme
+
+import (
+	_ "embed"
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/theme"
+	"image/color"
+)
+
+// 声明静态资源,在打包的时候导入到包里面
+var (
+	//go:embed fonts/simhei.ttf
+	NotoSansSC []byte
+)
+
+type MyTheme struct{}
+
+var _ fyne.Theme = (*MyTheme)(nil)
+
+// Font StaticName 为 fonts 目录下的 ttf 类型的字体文件名
+func (m MyTheme) Font(fyne.TextStyle) fyne.Resource {
+	return &fyne.StaticResource{
+		StaticName:    "simhei.ttf",
+		StaticContent: NotoSansSC,
+	}
+}
+
+func (*MyTheme) Color(n fyne.ThemeColorName, v fyne.ThemeVariant) color.Color {
+	return theme.DefaultTheme().Color(n, v)
+}
+
+func (*MyTheme) Icon(n fyne.ThemeIconName) fyne.Resource {
+	return theme.DefaultTheme().Icon(n)
+}
+
+func (*MyTheme) Size(n fyne.ThemeSizeName) float32 {
+	return theme.DefaultTheme().Size(n)
+}
